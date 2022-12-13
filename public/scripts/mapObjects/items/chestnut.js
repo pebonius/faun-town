cab953ecd5225bc25a04708fa34a25bf87b7a19b
peb <pebonius@gmail.com>
2022-12-13 23:06:20 +0100

    cleaning up scripts

--- public/scripts/mapObjects/items/chestnut.js
+++
@@ -1,9 +0,0 @@
-import Item from "./item.js";
-
-export default class Chestnut extends Item {
-  constructor(gameScreen, position, map) {
-    super(gameScreen, position, map);
-    this.name = "chestnut";
-    this.char = "🌰";
-  }
-}
--- public/scripts/mapObjects/items/toiletPaper.js
+++
@@ -1,9 +0,0 @@
-import Item from "./item.js";
-
-export default class ToiletPaper extends Item {
-  constructor(gameScreen, position, map) {
-    super(gameScreen, position, map);
-    this.name = "toilet paper";
-    this.char = "🧻";
-  }
-}
