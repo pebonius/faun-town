5db0e634db3aeb64561381bd3bc62430b49945eb
peb <pebonius@gmail.com>
2022-12-23 16:54:16 +0100

    playing action sequence in event

--- public/scripts/gameObjects/mapEvent.js
+++ public/scripts/gameObjects/mapEvent.js
@@ -10,6 +10,95 @@ export default class MapEvent extends Entity {
   }
   onCollision(collider) {
     super.onCollision(collider);
+    this.playActions();
+  }
+  playActions() {
+    if (Array.isArray(this.sequences) && this.sequences.length > 0) {
+      this.sequences.forEach((sequence) => {
+        const condition = this.getSupportedCondition(sequence.condition);
+        const conditionArgs = sequence.conditionArgs;
+
+        if (this.conditionFulfilled(condition, conditionArgs)) {
+          sequence.actions.forEach((action) => {
+            const supportedAction =
+              this.game.supportedActions.getSupportedAction(action.name);
+            supportedAction(...action.arguments);
+          });
+        }
+      });
+    }
+  }
+  conditionFulfilled(condition, conditionArgs) {
+    return (
+      condition != undefined &&
+      conditionArgs != undefined &&
+      condition(...conditionArgs)
+    );
+  }
+  getSupportedAction(action) {
+    switch (action) {
+      // case "restart":
+      //   return () => {
+      //     this.game.scenario.runScenario();
+      //   };
+      // case "nextScene":
+      //   return () => {
+      //     this.game.scenario.goToNextScene();
+      //   };
+      // case "goToScene":
+      //   return (id) => {
+      //     this.game.scenario.goToScene(id);
+      //   };
+      // case "addTopHud":
+      //   return (label, value) => {
+      //     this.game.scenario.addTopHudItem(label, value);
+      //   };
+      // case "setVariableValue":
+      //   return (name, value) => {
+      //     this.game.scenario.setVariableValue(name, value);
+      //   };
+      // case "changeVariableValue":
+      //   return (name, change) => {
+      //     this.game.scenario.changeVariableValue(name, change);
+      //   };
+      case "":
+        return () => {};
+      default:
+        return () => {
+          Debug.log(`tried to call unsupported action <<${action}>>.`);
+        };
+    }
+  }
+  getSupportedCondition(condition) {
+    switch (condition) {
+      // case "variableValueIs":
+      //   return (variableName, value) => {
+      //     return this.game.scenario.variableValueIs(variableName, value);
+      //   };
+      // case "variableValueIsNot":
+      //   return (variableName, value) => {
+      //     return this.game.scenario.variableValueIsNot(variableName, value);
+      //   };
+      // case "variableValueIsAtLeast":
+      //   return (variableName, value) => {
+      //     return this.game.scenario.variableValueIsAtLeast(variableName, value);
+      //   };
+      // case "variableValueIsNoMoreThan":
+      //   return (variableName, value) => {
+      //     return this.game.scenario.variableValueIsNoMoreThan(
+      //       variableName,
+      //       value
+      //     );
+      //   };
+      case "":
+        return () => {
+          return true;
+        };
+      default:
+        return () => {
+          Debug.log(`tried to call unsupported condition <<${condition}>>.`);
+        };
+    }
   }
   load(map, data) {
     this.name = data.name;
@@ -21,5 +110,7 @@ export default class MapEvent extends Entity {
     } else {
       this.sprite = this.gameScreen.content.pc;
     }
+
+    this.sequences = data.sequences;
   }
 }
