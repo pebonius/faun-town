cca8026a1b4921a83145cdd90a8008e97c6fd47b
pebonius <127553094+pebonius@users.noreply.github.com>
2023-04-23 10:23:58 +0200

    Update tilemap.js

--- public/scripts/gameObjects/tilemap.js
+++ public/scripts/gameObjects/tilemap.js
@@ -14,7 +14,7 @@ import MapEvent from "./mapEvent.js";
 export default class Tilemap {
   constructor(gameScreen, data) {
     this.gameScreen = gameScreen;
-    this.tileSize = new Point(64, 48);
+    this.tileSize = new Point(this.gamescreen.canvas.width / 10, this.gamescreen.canvas.height / 10);
     this.load(data);
   }
   toString() {
